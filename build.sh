e7a67b91d57fa38b6f24c5d8e68009cce20d6192
HUYNH Quang-Nghi <hqnghi88@gmail.com>
2017-03-29 22:26:37 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
-header=$(<msi.gama.experimental.parent/pom_header.xml)
+git_push(){
+    echo "git push new change of parent pom"
+    git config --global user.email "travis@travis-ci.com"
+    git config --global user.name "hqnghi88"
+    git config --global push.default simple
+    git remote rm origin
+    git remote add origin https://hqnghi88:$HQN_KEY@github.com/hqnghi88/gamaExpClone.git
+    git add -A
+    git commit -m "[ci skip] generate parent pom"
+    git push origin HEAD:master
+}
 
+header=$(<msi.gama.experimental.parent/pom_header.xml)
+current_modules=$(<msi.gama.experimental.parent/pom_modules.xml)
 footer=$(<msi.gama.experimental.parent/pom_footer.xml)
 
 modules=$'\n'$"<modules>"$'\n'
@@ -15,15 +27,8 @@ for file in *; do
 done
 
 modules="$modules </modules>"$'\n'
-echo "$modules" > msi.gama.experimental.parent/pom_modules.xml
-echo " $header $modules $footer " > msi.gama.experimental.parent/pom.xml
-
-
-git config --global user.email "travis@travis-ci.com"
-git config --global user.name "hqnghi88"
-git config --global push.default simple
-git remote rm origin
-git remote add origin https://hqnghi88:$HQN_KEY@github.com/hqnghi88/gamaExpClone.git
-git add -A
-git commit -m "[ci skip] generate parent pom"
-git push origin HEAD:master
+if [["$current_modules" != "$modules" ]]; then
+    echo "$modules" > msi.gama.experimental.parent/pom_modules.xml
+    echo " $header $modules $footer " > msi.gama.experimental.parent/pom.xml
+    git_push()
+if

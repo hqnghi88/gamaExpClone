3bb4a513d2c6513a746fe6e96d2ae0e7b2ce03ea
HUYNH Quang-Nghi <hqnghi88@gmail.com>
2017-03-29 21:17:33 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,5 +2,8 @@
 for file in *; do 
   if [[ -d "$file" && ! -L "$file" ]]; then
     echo "$file is a directory"; 
+    if [ -f "$file/pom.xml" ]; then
+        echo "File found!"
+    fi
   fi; 
 done

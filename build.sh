33e72c1aa12c3f8d8cd8466472a0e819d6360803
HUYNH Quang-Nghi <hqnghi88@gmail.com>
2017-03-29 21:25:19 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+modules = "<modules>\n"
 for file in *; do 
   if [[ -d "$file" && ! -L "$file" ]]; then
     echo "$file is a directory"; 
     if [ -f "$file/pom.xml" ]; then
-        echo "File found!"
+        echo "File $file/pom.xml found!"        
+        modules="$modules <module>$file/pom.xml</module>\n"
     fi
   fi; 
+modules="$modules </modules>"
+echo $modules
 done
